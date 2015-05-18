c1ea7d47aa45ff898aeece15253352a50f487fbf
Ryan May <mryanmay@gmail.com>
2015-05-17 23:32:26 -0600

    v2. Using global object rather than copying/passing arrays.

--- ruby/fixnum_centipad/Centipad.rb
+++ ruby/fixnum_centipad/Centipad.rb
@@ -1,30 +1,50 @@
-# simple (read: dumb) recursion algorithm. Lots of room for optimization.
+# Improves on V1 by maintaining a global object rather than passing a new array to each recursive call.
+# Should reduce memory required significantly which should allow computing larger data sets.
 
 class Centipad
-  def solve()
+  def initialize(min=1, max=9, equal=100, print=false)
+    @equal = equal
+    @print = print
+    @current_state = (min..max).to_a
+    @current_location = 1
     @solutions = 0
+  end
+
+  def solve()
     beginning_time = Time.now
-    start = (1..9).to_a
-    loop(start)
+    loop()
     end_time = Time.now
     puts @solutions.to_s
     puts "Time elapsed #{(end_time - beginning_time)*1000} milliseconds"
   end
-  def loop(current_array, current_location=1)
-    if current_location >= current_array.size
+
+  def loop()
+    if @current_location >= @current_state.size
       # Evaluate and print
-      if eval(current_array.join()) == 100
-        # puts current_array.join()
+      if eval(@current_state.join()) == @equal
+        puts @current_state.join() if @print
         @solutions += 1
       end
       return
     else
       # Concat
-      loop(current_array, current_location+1)
+      @current_location += 1
+      loop()
+      @current_location -= 1
+
       # Add
-      loop(current_array.dup.insert(current_location, '+'), current_location+2)
+      @current_state.insert(@current_location, '+')
+      @current_location += 2
+      loop()
+      @current_location -= 2
+      @current_state.delete_at(@current_location)
+
       # Subtract
-      loop(current_array.dup.insert(current_location, '-'), current_location+2)
+      @current_state.insert(@current_location, '-')
+      @current_location += 2
+      loop()
+      @current_location -= 2
+      @current_state.delete_at(@current_location)
     end
   end
 end
